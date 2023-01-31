967a8bd4ee56bcfe676aa96c8904fd448b4fe089
“DaiaUrq” <daiaurquilla@gmail.com>
2023-01-31 16:34:48 -0300

    back agregado direccion del front

--- src/main/java/com/portfolio/DaiaUrquilla/Controller/ProyectoController.java
+++ src/main/java/com/portfolio/DaiaUrquilla/Controller/ProyectoController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://frontportfoliodu.web.app" ,"http://localhost:4200"})
 public class ProyectoController {
 
     @Autowired
