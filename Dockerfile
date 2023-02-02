51cd13a51d54564a23af10fce8cd43678911d0b5
“DaiaUrq” <daiaurquilla@gmail.com>
2023-02-02 17:12:59 -0300

    properties

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM amazoncorretto:17-alpine-jdk
+FROM openjdk:17-jdk-slim
+MAINTAINER daiaurquilla
 COPY target/DaiaUrquilla-0.0.1-SNAPSHOT.jar  app.jar
 ENTRYPOINT ["java","-jar", "/app.jar"]
--- src/main/java/com/portfolio/DaiaUrquilla/Security/jwt/JwtEntryPoint.java
+++ src/main/java/com/portfolio/DaiaUrquilla/Security/jwt/JwtEntryPoint.java
@@ -21,9 +21,11 @@ public class JwtEntryPoint implements AuthenticationEntryPoint{
     private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
         logger.error("Falló el metodo commence ");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
     
     
